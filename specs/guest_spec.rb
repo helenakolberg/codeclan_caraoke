ea60483c081a32b3dd8e7f2bbd542f438a85bf42
Helena <inger.helena@gmail.com>
2020-06-26 14:41:11 +0100

    test name pass

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -7,9 +7,11 @@ require_relative('../guest')
 class TestGuest < Minitest::Test
 
     def setup()
-        guest = Guest.new("Jonathan")
+        @guest = Guest.new("Jonathan")
     end
 
-    
+    def test_name()
+        assert_equal("Jonathan", @guest.name)
+    end
 
 end
