771745db47575e1e7b9a2b2aed790c1e9d631a5c
Helena <inger.helena@gmail.com>
2020-06-26 16:03:54 +0100

    added money property, test money pass

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -7,11 +7,15 @@ require_relative('../guest')
 class TestGuest < Minitest::Test
 
     def setup()
-        @guest = Guest.new("Jonathan")
+        @guest = Guest.new("Jonathan", 100)
     end
 
     def test_name()
         assert_equal("Jonathan", @guest.name)
+    end
+
+    def test_money()
+        assert_equal(100, @guest.money)
     end
 
 end
